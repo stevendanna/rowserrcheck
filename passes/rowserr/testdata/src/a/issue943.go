3563e895b5ce12cf218d8e04c279ec1acd03d346
gaojingyu <1121087373@qq.com>
2021-01-30 02:41:56 +0800

    add testcase (#12)
    * fix:fix false positive report phi instruction
    
    * add test
    
    Co-authored-by: = <=>

--- passes/rowserr/testdata/src/a/issue943.go
+++ passes/rowserr/testdata/src/a/issue943.go
@@ -33,6 +33,29 @@ func issue943_1() {
 	}
 }
 
+func issue943_11() {
+	db, err := sql.Open("postgres", "postgres://localhost:5432/postgres")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	var rows *sql.Rows
+	if rand.Float64() < 0.5 {
+		rows, err = db.Query("select 1") // want "rows.Err must be checked"
+	} else {
+		rows, err = db.Query("select 2") // want "rows.Err must be checked"
+	}
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		fmt.Println("new rows")
+	}
+
+}
+
 func issue943_2() {
 	db, err := sql.Open("postgres", "postgres://localhost:5432/postgres")
 	if err != nil {
@@ -45,4 +68,16 @@ func issue943_2() {
 	if err := rows.Err(); err != nil {
 		panic(err)
 	}
+}
+
+func issue943_22() {
+	db, err := sql.Open("postgres", "postgres://localhost:5432/postgres")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	rows, _ := db.Query("select 1") // want "rows.Err must be checked"
+	defer rows.Close()
+
 }
